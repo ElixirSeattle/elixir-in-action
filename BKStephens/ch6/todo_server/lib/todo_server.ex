9ecee80454807d22a590e9dec919cadada68d4a0
Ben Stephens <BKStephens@outlook.com>
2016-09-05 18:36:28 -0700

    Refactoring TodoServer to use GenServer

--- BKStephens/ch6/todo_server/lib/todo_server.ex
+++ BKStephens/ch6/todo_server/lib/todo_server.ex
@@ -1,48 +1,48 @@
 defmodule TodoServer do
-  import ServerProcess
+  use GenServer
   import TodoList
 
   #### External API
 
   def start do
-    ServerProcess.start(TodoServer)
+    GenServer.start(TodoServer, nil)
   end
 
   def entries(todo_server, date) do
-    ServerProcess.call(todo_server, {:entries, date})
+    GenServer.call(todo_server, {:entries, date})
   end
 
   def add_entry(todo_server, new_entry) do
-    ServerProcess.cast(todo_server, {:add_entry, new_entry})
+    GenServer.cast(todo_server, {:add_entry, new_entry})
   end
 
   def update_entry(todo_server, entry) do
-    ServerProcess.cast(todo_server, {:update_entry, entry})
+    GenServer.cast(todo_server, {:update_entry, entry})
   end
 
   def delete_entry(todo_server, entry) do
-    ServerProcess.cast(todo_server, {:delete_entry, entry})
+    GenServer.cast(todo_server, {:delete_entry, entry})
   end
 
-  #### ServerProcess API
+  #### GenServer API
 
-  def init do
-    TodoList.new
+  def init(_) do
+    {:ok, TodoList.new}
   end
 
   def handle_cast({:add_entry, new_entry}, todo_list) do
-    TodoList.add_entry(todo_list, new_entry)
+    {:noreply, TodoList.add_entry(todo_list, new_entry)}
   end
 
   def handle_cast({:update_entry, entry}, todo_list) do
-    TodoList.update_entry(todo_list, entry)
+    {:noreply, TodoList.update_entry(todo_list, entry)}
   end
 
   def handle_cast({:delete_entry, entry}, todo_list) do
-    TodoList.delete_entry(todo_list, entry)
+    {:noreply, TodoList.delete_entry(todo_list, entry)}
   end
 
-  def handle_call({:entries, date}, todo_list) do
-    {TodoList.entries(todo_list, date), todo_list}
+  def handle_call({:entries, date}, _, todo_list) do
+    {:reply, TodoList.entries(todo_list, date), todo_list}
   end
 end
--- BKStephens/ch6/todo_server/test/todo_server_test.exs
+++ BKStephens/ch6/todo_server/test/todo_server_test.exs
@@ -2,14 +2,14 @@ defmodule TodoServerTest do
   use ExUnit.Case
 
   test "add_entry" do
-    pid = TodoServer.start
+    {:ok, pid} = TodoServer.start
     TodoServer.add_entry(pid, %{date: {2016, 9, 5}, title: "Write tests"})
     TodoServer.add_entry(pid, %{date: {2016, 9, 6}, title: "Profit"})
     assert [%{date: {2016, 9, 5}, id: 1, title: "Write tests"}] == TodoServer.entries(pid, {2016, 9, 5})
   end
 
   test "update_entry" do
-    pid = TodoServer.start
+    {:ok, pid} = TodoServer.start
     TodoServer.add_entry(pid, %{date: {2016, 9, 5}, title: "Write tests"})
     [%{id: id}] = TodoServer.entries(pid, {2016, 9, 5})
     TodoServer.update_entry(pid, %{id: id, title: "Write tests!"})
@@ -17,7 +17,7 @@ defmodule TodoServerTest do
   end
 
   test "delete_entry" do
-    pid = TodoServer.start
+    {:ok, pid} = TodoServer.start
     TodoServer.add_entry(pid, %{date: {2016, 9, 5}, title: "Write tests"})
     [%{id: id}] = TodoServer.entries(pid, {2016, 9, 5})
     TodoServer.delete_entry(pid, %{id: id})
